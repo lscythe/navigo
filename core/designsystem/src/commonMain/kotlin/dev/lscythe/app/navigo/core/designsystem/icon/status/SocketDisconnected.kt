33e3e66a2437bbe933b3b674db39356794bf1e0e
Lscythe <lscythe.git@proton.me>
2026-09-14 15:27:13 +0700

    feat(core:designsystem): add socket status icons

---
+++ core/designsystem/src/commonMain/kotlin/dev/lscythe/app/navigo/core/designsystem/icon/status/SocketDisconnected.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2026 Lscythe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.lscythe.app.navigo.core.designsystem.icon.status
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import dev.lscythe.app.navigo.core.designsystem.icon.NavigoIcons
+
+val NavigoIcons.SocketDisconnected: ImageVector
+    get() {
+        if (_SocketDisconnected != null) {
+            return _SocketDisconnected!!
+        }
+        _SocketDisconnected =
+            ImageVector.Builder(
+                    name = "SocketDisconnected",
+                    defaultWidth = 24.dp,
+                    defaultHeight = 24.dp,
+                    viewportWidth = 24f,
+                    viewportHeight = 24f,
+                )
+                .apply {
+                    path(
+                        stroke = SolidColor(Color.Black),
+                        strokeLineWidth = 2f,
+                        strokeLineCap = StrokeCap.Round,
+                        strokeLineJoin = StrokeJoin.Round,
+                    ) {
+                        moveToRelative(20f, 16f)
+                        lineToRelative(-4f, 4f)
+                        moveToRelative(-9f, -8f)
+                        lineToRelative(5f, 5f)
+                        lineToRelative(-1.5f, 1.5f)
+                        arcToRelative(
+                            3.536f,
+                            3.536f,
+                            0f,
+                            isMoreThanHalf = true,
+                            isPositiveArc = true,
+                            -5f,
+                            -5f,
+                        )
+                        close()
+                        moveTo(17f, 12f)
+                        lineToRelative(-5f, -5f)
+                        lineToRelative(1.5f, -1.5f)
+                        arcToRelative(
+                            3.536f,
+                            3.536f,
+                            0f,
+                            isMoreThanHalf = true,
+                            isPositiveArc = true,
+                            5f,
+                            5f,
+                        )
+                        close()
+                        moveTo(3f, 21f)
+                        lineToRelative(2.5f, -2.5f)
+                        moveToRelative(13f, -13f)
+                        lineTo(21f, 3f)
+                        moveToRelative(-11f, 8f)
+                        lineToRelative(-2f, 2f)
+                        moveToRelative(5f, 1f)
+                        lineToRelative(-2f, 2f)
+                        moveToRelative(5f, 0f)
+                        lineToRelative(4f, 4f)
+                    }
+                }
+                .build()
+
+        return _SocketDisconnected!!
+    }
+
+@Suppress("ObjectPropertyName") private var _SocketDisconnected: ImageVector? = null
